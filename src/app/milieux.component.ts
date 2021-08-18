de4080bd149a6ecf5d778e04d9fcf6cd1865fef7
Ali Akbar Shahi <aliakbarshahice@gmail.com>
2021-08-18 11:30:37 +0430

    fix style and root and names

---
+++ src/app/milieux.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'milieux-root',
+  templateUrl: './milieux.component.html',
+  styleUrls: ['./milieux.component.scss'],
+})
+export class MilieuxComponent {
+  title = 'milieux-workspace';
+}
---
+++ src/app/milieux.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { MilieuxComponent } from './milieux.component';
+
+@NgModule({
+  declarations: [MilieuxComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [MilieuxComponent],
+})
+export class MilieuxModule {}
